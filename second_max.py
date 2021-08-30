c19973c82cfc618efdbe937c8fd0d93cede438f9
genndy007 <kochevgenazp@gmail.com>
2021-08-30 06:16:52 +0300

    Fix problem in my implementation

--- second_max.py
+++ second_max.py
@@ -8,12 +8,14 @@ def second_max_myself(A: List[int]) -> int:
     if len(A) < 2:
         raise Exception('Cannot find second in such little array')
 
-    (cur_max, cur_second) = (A[0], A[1]) if A[0] > A[1] else (A[1], A[0])
+    cur_max = cur_second = -float('inf')
 
-    for unit in A:
-        if unit > cur_max:
+    for i in range(len(A)):
+        if A[i] > cur_max:
             cur_second = cur_max
-            cur_max = unit
+            cur_max = A[i]
+        elif A[i] > cur_second:
+            cur_second = A[i]
 
     return cur_second
 
@@ -24,5 +26,17 @@ def second_max_myself(A: List[int]) -> int:
 # print(t1)
 
 
-t2 = second_max_myself([1,4,6,7,3,4,6])
-print(t2)
+# t2 = second_max_myself([1,4,6,7,3,4,6])
+# print(t2)
+
+tests = [
+    [1, 2],
+    [4, 5, 6, 7, 18, 2],
+    [5, 5, 5, 5],
+    [17, 3, 2, 1],
+    [17, 17, 1],
+    [1, 1, 17]
+]
+
+for test in tests:
+    print(test, second_max_myself(test))
